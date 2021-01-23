91abca843a701944393e0ad6f8541c2011a78057
Himangshu De <dehimangshu2020@gmail.com>
2021-01-23 12:30:50 +0530

    Added Workspace.py file 23012021

---
+++ Workspace.py
@@ -0,0 +1,35 @@
+# list1 = [65,45,65,57,82,33,99,44]
+# largest = list1[0]
+# second_largest = list1[0]
+# smallest = list1[0]
+# for num in list1:
+#     if num>second_largest and num<largest:
+#         second_largest = num
+
+#     if num >largest:
+#         largest = num
+
+
+
+# print(f"The largest number is: {largest}")
+# print(f"The second largest number is: {second_largest}")
+# # print(f"The smallest number is: {smallest}")
+
+
+# from Functions import palindrome
+# palindrome(101)
+
+from Functions import *
+# user = int(input("Choose one:\n1. Word Palindrome\n2. Number Palindrome\n"))
+
+# if user == 1:
+#     str1 = input("Enter word: ")
+#     ab = palindrome_str(str1)
+#     if ab == True:
+#         print(f'Goodluck')
+    
+# if user == 2:
+#     num1 = int(input("Enter number: "))
+#     ac = palindrome_num(num1)
+#     if ac == True:
+#         print(num1/2)
