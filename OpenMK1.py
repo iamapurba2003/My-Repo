fe0de0620a45ed16a37391608af7ee6a19ff873b
Himangshu De <dehimangshu2020@gmail.com>
2021-01-27 18:34:05 +0530

    Added New files and modified openmk1.py 27012021

--- OpenMK1.py
+++ OpenMK1.py
@@ -1,17 +1,43 @@
 import os
 import webbrowser
 
-print('Hello! I can open or search for you just type path variable or web address...')
-
 while True:
-    i = input("")
-    if i == "":
-        break
-    else:
-        try:
-            os.startfile(i)
-        except FileNotFoundError:
-            print("Can't Find the file according to the given path")
+    print("What will you open?")
+    print("1. File using the file address\n2. Weblink or\n3. Exit this Program")
+    try:
+        inpt = int(input("Enter option of your choice: "))
+    
+        if inpt == "":
+            print("Have a good day ahead!!")
+            break
+        if inpt == 1:
+            try:
+                addr = input("Enter the file address: ")
+                if addr == "":
+                    print("Have a good day ahead!!")
+                    break
+                else:
+                    os.startfile(addr)
+                    print('-'*50)
+            except FileNotFoundError:
+                print("Can't find file according to path specified.")
+                
+        if inpt == 2:
+            website = input("Enter the web address: ")
+            if website == "":
+                print("Have a good day ahead!!")
+                break
+            else:
+                webbrowser.open_new_tab(website)
+                print('-'*50)
+        if inpt == 3:
+            print("Have a good day!!")
+            break
+    except ValueError:
+        print("Please enter numeric input only.")
+        print()
+        print('-'*50)
+            
             
 
 
---
+++ Task_Performer_v_1.2.py
@@ -0,0 +1,134 @@
+try:
+    import pywhatkit as kit
+    #install pywhatkit if not installed earlier
+    
+    import smtplib
+
+    import os
+
+    import webbrowser as wb
+
+    try:
+        while True:
+            print('Hello, What task do you want to perform?\n1. Send a WhatsApp Message\n2. Play on YouTube\n3. Search on Web\n4. Search on Wikipedia\n5. Open files on this computer\n6. Shut Down the system\n7. Open any web link\n8. Send an Email\nOr simply press Enter to exit...')
+            print()
+            print("Enter the option no. from above:")
+            option = int(input(""))
+    
+            if option == 1:
+                print('Ok...')
+                dial_code = str(input("Enter the country's dialling code(ignore (+)): "))
+                contact = str(input("Enter receiver's number: "))
+                msg = str(input('Enter the message: '))
+                time1 = int(input("Enter the time(hour unit): "))
+                time2 = int(input("Enter the time(min. unit): "))
+                delay = int(input("Set delay(min:10): "))
+                dial = ("+"+dial_code+contact)
+                kit.sendwhatmsg(dial, msg, time1, time2, delay)
+                #kit.sendwhatmsg() is receiver number, msg, hr, min,delay,sleep
+
+            if option == 2:
+                print('Enter the keyword:')
+                keyword = str(input())
+                print("Opening on YouTube")
+                kit.playonyt(keyword)
+                print()
+
+            if option == 3:
+                print("Ok... Enter the keyword:")
+                searchword = str(input())
+                print("Opening Google and searching")
+                kit.search(searchword)
+                print()
+            
+            if option == 4:
+                print("Ok... Enter the keyword to search: ")
+                while True:
+                    
+                    wikisearch = input()
+                    if wikisearch == "":
+                        break
+                    else:    
+                        print('Searching...')
+                        print()
+                    try:
+                        kit.info(wikisearch)
+                        break
+                    except Exception:
+                        print("Error! Try using other keywords...")
+                        continue
+                
+            if option == 5:
+                print('Enter the file path:')
+                path = input("")
+                try:
+                    print("Opening...")
+                    os.startfile(path)
+                except FileNotFoundError:
+                    print("Can't find the file according to the given path")
+                    print()
+
+            if option == 6:
+                print("Ok... Set the countdown timer(secs):")
+                countdown = int(input())
+                kit.shutdown(countdown)
+                print('Your system will shut down in',countdown,'seconds')
+                print()
+                print("Do you want to cancel scheduled shutdown?\n1. Yes\n2. No")
+                cancel = int(input('Enter numeric input: '))
+                if cancel == 1:
+                    kit.cancelShutdown()
+                    print("Scheduled ShutDown cancelled...")
+                    print()
+                else:
+                    print()              
+                    continue
+
+            if option == 7:
+                print("Enter the link: ")
+                link1 = input()
+                print("Opening link...")
+                wb.open_new_tab(link1)
+                print()
+            
+            if option == 8:
+                print("To use this service, you would need to turn on 'Less Secure app access' under security tab of your google account")
+                print("CAUTION: If you have enabld two step verification then disable it...")
+                while True:
+                    try:
+                        user_info = input("Enter your mail address: ")
+                        user_pass = input("Enter your password: ")
+                        mailAddress = input("Enter recipient's mail address: " )
+                        text = input("Enter message to be sent: ")
+
+
+                        server = smtplib.SMTP('smtp.gmail.com', 587)
+                        server.starttls()
+                        server.login(user_info, user_pass)
+                        server.sendmail(user_info , mailAddress, text)
+                        print("Voila! Your mail was sent successfully...")
+                        break
+                        
+                    except Exception:
+                        print("Your mail was not \"Sent\" due to some potential errors...")
+                        break
+
+
+
+
+
+
+            print('Wanna perform any other task again?\n1. Yes\n2. No')
+            run = int(input('Enter option number: '))
+            if run == 1:
+                print('Ok')
+                print('-'*25)
+                continue
+                
+            if run == 2:
+                print("Have a good day!!")
+                break
+    except ValueError:
+        print("Have a good day!!")
+except ModuleNotFoundError:
+    print('INSTALL REQUIRED MODULES: pywhatkit')
