46e2723cdb8eddd95632eba6821fa677938a02f5
Himangshu De <dehimangshu2020@gmail.com>
2021-02-03 19:29:16 +0530

    Added System Play functionality in TIC TAC TOE.py 1929_03022021

--- TIC TAC TOE.py
+++ TIC TAC TOE.py
@@ -11,21 +11,40 @@
 
 
 #------------------! Main Code !----------------------
+#-------! Modules !---------
+import random
+
 #----------! Global Variables !------------
-player_1 = input("Enter Player 1 Name: ")
-choice_player1 = input("Enter Player 1's Choice (X / O): ")
-choice_2 = "X"
-if choice_player1 == "X":
-    choice_2 = "O"
-else:
-    choice_2 = choice_2
-player_2 = input("Enter player 2 Name: ")
-pos = ['1', '2', '3', '4', '5', '6', '7', '8','9']
 
+play_choice = int(input("1.PvP\n2.PvSys\nEnter num choice: "))
+pos = ['', ' ', '', ' ', ' ', '', '', ' ',' ']
 
+GREEN = '\u001b[32m'
+RESET = '\u001b[0m'
+BLUE = '\u001b[34m'
+BOLD = '\u001b[1m'
+RED = '\u001b[31m'
+YELLOW = '\u001b[33m'
 
+if play_choice == 1:
+    player_1 = input("Enter Player 1 Name: ")
+    choice_player1 = input("Enter Player 1's Choice (X / O): ")
+    choice_2 = "X"
+    if choice_player1 == "X":
+        choice_2 = "O"
+    else:
+        choice_2 = choice_2
+    player_2 = input("Enter player 2 Name: ")
 
-
+if play_choice == 2:
+    player = input("Enter player's name: ")
+    sys_choice = "X"
+    choice_player = input("Enter choice (X / O): ")
+    if choice_player == 'X':
+        sys_choice = "O"
+    else:
+        sys_choice = sys_choice
+        
 
 #To display board...
 def board():
@@ -33,21 +52,21 @@ def board():
     i = 0
     while i <19:
         if i == 0:
-            print('-'*49)
+            print(f'{BLUE}-{RESET}'*49)
         else:
-            print(f"| \t  \t | \t  \t |\t  \t|")
+            print(f"{BLUE}| \t  \t | \t  \t |\t  \t|{RESET}")
         if i == 3:
-            print(f"| \t {pos[0]} \t | \t {pos[1]} \t |\t {pos[2]} \t|")
+            print(f"{BLUE}| \t {GREEN}{pos[0]}{RESET}{BLUE} \t | \t {GREEN}{pos[1]}{RESET}{BLUE} \t |\t {GREEN}{pos[2]}{RESET}{BLUE} \t|{RESET}")
         if i == 6:
-            print('-'*49)
+            print(f'{BLUE}-{RESET}'*49)
         if i == 9:
-            print(f"| \t {pos[3]} \t | \t {pos[4]} \t |\t {pos[5]} \t|")
+            print(f"{BLUE}| \t {GREEN}{pos[3]}{RESET}{BLUE} \t | \t {GREEN}{pos[4]}{RESET}{BLUE} \t |\t {GREEN}{pos[5]}{RESET}{BLUE} \t|{RESET}")
         if i == 12:
-            print('-'*49)
+            print(f'{BLUE}-{RESET}'*49)
         if i == 15:
-            print(f"| \t {pos[6]} \t | \t {pos[7]} \t |\t {pos[8]} \t|")
+            print(f"{BLUE}| \t {GREEN}{pos[6]}{RESET}{BLUE} \t | \t {GREEN}{pos[7]}{RESET}{BLUE} \t |\t {GREEN}{pos[8]}{RESET}{BLUE} \t|{RESET}")
         if i == 18:
-            print('-'*49)
+            print(f'{BLUE}-{RESET}'*49)
             
         i+=1
 
@@ -55,48 +74,48 @@ def board():
 #Checking for a winner
 def winner():
     #horizontal check for "X"
-    if pos[0] == pos[1] == pos[2] == "X":
+    if pos[0] == pos[1] == pos[2]:# == "X":
         return True
-    elif pos[3] == pos[4] == pos[5] == "X":
-        return True
-    elif pos[6] == pos[7] == pos[8] == "X":
-        return True
+    # elif pos[3] == pos[4] == pos[5] == "X":
+    #     return True
+    # elif pos[6] == pos[7] == pos[8] == "X":
+    #     return True
     
-    #horizontal check for "O"
-    elif pos[0] == pos[1] == pos[2] == "O":
-        return True
-    elif pos[3] == pos[4] == pos[5] == "O":
-        return True
-    elif pos[6] == pos[7] == pos[8] == "O":
-        return True
+    # #horizontal check for "O"
+    # elif pos[0] == pos[1] == pos[2] == "O":
+    #     return True
+    # elif pos[3] == pos[4] == pos[5] == "O":
+    #     return True
+    # elif pos[6] == pos[7] == pos[8] == "O":
+    #     return True
     
     #vertical check for "X"
-    elif pos[0] == pos[3] == pos[6] == "X":
+    elif pos[0] == pos[3] == pos[6]: #== "X":
         return True
-    elif pos[1] == pos[4] == pos[7] == "X":
-        return True
-    elif pos[2] == pos[5] == pos[8] == "X":
-        return True
+    # elif pos[1] == pos[4] == pos[7] == "X":
+    #     return True
+    # elif pos[2] == pos[5] == pos[8] == "X":
+    #     return True
     
-    #vertical check for "O"
-    elif pos[0] == pos[3] == pos[6] == "O":
-        return True
-    elif pos[1] == pos[4] == pos[7] == "O":
-        return True
-    elif pos[2] == pos[5] == pos[8] == "O":
-        return True
+    # #vertical check for "O"
+    # elif pos[0] == pos[3] == pos[6] == "O":
+    #     return True
+    # elif pos[1] == pos[4] == pos[7] == "O":
+    #     return True
+    # elif pos[2] == pos[5] == pos[8] == "O":
+    #     return True
     
     #Diagonal check for "X"
-    elif pos[0] == pos[4] == pos[8] == "X":
+    elif pos[0] == pos[4] == pos[8]: #== "X":
         return True
-    elif pos[2] == pos[4] == pos[6] == "X":
-        return True
+    # elif pos[2] == pos[4] == pos[6] == "X":
+    #     return True
 
     #Diagonal check for "O"
-    elif pos[0] == pos[4] == pos[8] == "O":
+    elif pos[0] == pos[4] == pos[8]: #== "O":
         return True
-    elif pos[2] == pos[4] == pos[6] == "O":
-        return True
+    # elif pos[2] == pos[4] == pos[6] == "O":
+    #     return True
     else:
         return None
 
@@ -115,56 +134,130 @@ def swap_players():
     i = 1
     overall = winner()
     while i<10:
-        if i%2 != 0:
-            board()
-            print("{}'s turn".format(player_1))
-            player1_pos = int(input("Enter position from 1 to 9: ")) - 1
-            write = overwrite(player1_pos)
-            while write == True:
-                if write == True:
-                    player1_pos = int(input()) -1
-                    write = overwrite(player1_pos)
-                else:
-                    break
-            pos[player1_pos] = choice_player1
-            win = winner()
-            if win == True:
+        if play_choice == 1:
+            if i%2 != 0:
                 board()
-                print(f"{player_1} wins the game!")
-                break
-            
-            
-        if i%2 == 0:
-            board()
-            print(f"{player_2}'s turn")
-            player2_pos = int(input("Enter position from 1 to 9: ")) - 1 
-            write = overwrite(player2_pos)
-            while write == True:
-                if write == True:
-                    player2_pos = int(input()) -1
-                    write = overwrite(player2_pos)
-                else:
+                print("{}'s turn".format(player_1))
+                player1_pos = int(input("Enter position from 1 to 9: ")) - 1
+                write = overwrite(player1_pos)
+                while write == True:
+                    if write == True:
+                        player1_pos = int(input()) -1
+                        write = overwrite(player1_pos)
+                    else:
+                        break
+                pos[player1_pos] = choice_player1
+                win = winner()
+                if win == True:
+                    board()
+                    print(f"{RED}{BOLD}{player_1}{RESET} wins the game!")
                     break
-            pos[player2_pos] = choice_2
-            win2 = winner()
-            if win2 == True:
+                
+                
+            if i%2 == 0:
                 board()
-                print(f"{player_2} wins the game!")
-                break
-
+                print(f"{player_2}'s turn")
+                player2_pos = int(input("Enter position from 1 to 9: ")) - 1 
+                write = overwrite(player2_pos)
+                while write == True:
+                    if write == True:
+                        player2_pos = int(input()) -1
+                        write = overwrite(player2_pos)
+                    else:
+                        break
+                pos[player2_pos] = choice_2
+                win2 = winner()
+                if win2 == True:
+                    board()
+                    print(f"{YELLOW}{BOLD}{player_2}{RESET} wins the game!")
+                    break
+        if play_choice == 2:
+            if choice_player == "X":
+                if i%2 != 0:
+                    board()
+                    print("{}'s turn".format(player))
+                    player_pos = int(input("Enter position from 1 to 9: ")) - 1
+                    write = overwrite(player_pos)
+                    while write == True:
+                        if write == True:
+                            player_pos = int(input()) -1
+                            write = overwrite(player_pos)
+                        else:
+                            break
+                    pos[player_pos] = choice_player
+                    win = winner()
+                    if win == True:
+                        board()
+                        print(f"{BOLD}{GREEN}{player}{RESET} wins the game!")
+                        break
+            
+                if i%2 == 0:
+                    board()
+                    print("System's turn")
+                    sys_turn = random.randint(1, 10)
+                    write = overwrite(sys_turn)
+                    while write == True:
+                        if write == True:
+                            sys_turn = random.randint(1,10)
+                            write = overwrite(sys_turn)
+                        else:
+                            break
+                    print("System's turn complete...")
+                    pos[sys_turn] = sys_choice
+                    win = winner()
+                    if win == True:
+                        board()
+                        print(f"{BOLD}{RED}Computer{RESET} wins the game!")
+                
+            if choice_player == "O":
+                if i%2 == 0:
+                    board()
+                    print("{}'s turn".format(player))
+                    player_pos = int(input("Enter position from 1 to 9: ")) - 1
+                    write = overwrite(player_pos)
+                    while write == True:
+                        if write == True:
+                            player_pos = int(input()) -1
+                            write = overwrite(player_pos)
+                        else:
+                            break
+                    pos[player_pos] = choice_player
+                    win = winner()
+                    if win == True:
+                        board()
+                        print(f"{BOLD}{GREEN}{player}{RESET} wins the game!")
+                        break
+            
+                if i%2 != 0:
+                    board()
+                    print("System's turn")
+                    sys_turn = random.randint(1, 10)
+                    write = overwrite(sys_turn)
+                    while write == True:
+                        if write == True:
+                            sys_turn = random.randint(1,10)
+                            write = overwrite(sys_turn)
+                        else:
+                            break
+                    print("System's turn complete...")
+                    pos[sys_turn] = sys_choice
+                    win = winner()
+                    if win == True:
+                        board()
+                        print(f"{BOLD}{RED}Computer{RESET} wins the game!")
 
         if i == 9 and overall == None:
             board()
-            print("It's a TIE!")
+            print("No one wins :( !")
             
         i+=1
 
 
 #main game
 def game():
+    
     swap_players()
     
-
 
 
 game()
