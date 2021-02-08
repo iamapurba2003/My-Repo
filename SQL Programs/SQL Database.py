42fb05c0258e374b55353ef8b47cc8732ea8b2d1
Himangshu De <dehimangshu2020@gmail.com>
2021-02-08 20:58:19 +0530

    Modified SQL Database.py program with latest updates 2059_08022021

--- SQL Programs/SQL Database.py
+++ SQL Programs/SQL Database.py
@@ -1,6 +1,72 @@
 """
-Write a code to 
+Write a code to create a database in MySQL
 """
+
+import mysql.connector 
+db = mysql.connect(host = "localhost", user = "root", password = "root", auth_plugins = "mysql_native_password")
+myCursor = db.cursor()
+
+
+
+
+
+
+"""
+Write a Program to Create five tables in the database created in the previous file.
+"""
+
+import mysql.connector 
+db = mysql.connect(host = "localhost", user = "root", password = "root", auth_plugins = "mysql_native_password")
+myCursor = db.cursor()
+
+Q1 = "CREATE TABLE table1(name VARCHAR(50), class int(5), sec(5))"
+Q2 = "CREATE TABLE table1(name VARCHAR(50), class int(5), sec(5))"
+Q3 = "CREATE TABLE table1(name VARCHAR(50), class int(5), sec(5))"
+Q4 = "CREATE TABLE table1(name VARCHAR(50), class int(5), sec(5))"
+Q5 = "CREATE TABLE table1(name VARCHAR(50), class int(5), sec(5))"
+
+myCursor.execute(Q1)
+myCursor.execute(Q2)
+myCursor.execute(Q3)
+myCursor.execute(Q4)
+myCursor.execute(Q5)
+
+db.commit()
+myCursor.execute("SHOW TABLES")
+for x in myCursor:
+    print(x)
+
+
+
+"""
+Write a program to insert 5 records in any one of the table created 
+"""
+
+
+import mysql.connector 
+db = mysql.connect(host = "localhost", user = "root", password = "root", auth_plugins = "mysql_native_password")
+myCursor = db.cursor()
+
+myCursor.execute("INSERT INTO table1(name,class, sec) VALUES(%s, %s, %s)")
+userData = [
+    ("Apurba Ghosh", 11, "C"),
+    ("Himangshu De", 11, "C"),
+    ("Raima Ray", 11, "B"),
+    ("Priyanka Das", 11, "D"),
+    ("Rupankar Das", 11, "C")
+]
+
+#myCursor
+for x in userData:
+    myCursor.execute(Q1, x)
+
+myCursor.execute("SELECT * FROM table1 OREDER BY sec ASC/DESC ")
+for x in myCursor:
+    print(x)
+
+
+
+
 
 
 
