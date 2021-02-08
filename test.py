8e8a656dee6aed912c45d2d022bae7db8d79023d
Himangshu De <dehimangshu2020@gmail.com>
2021-02-08 20:15:58 +0530

    Changed test.py 2015_08022021

--- test.py
+++ test.py
@@ -1,7 +1,28 @@
-import pyaudio
+import pyrebase
+
+firebaseConfig = {'apiKey': "AIzaSyDdksFXF_bDbBIYi7Nvpeo8YQvJW80U27s",
+    'authDomain': "test-case-88b02.firebaseapp.com",
+    'projectId': "test-case-88b02",
+    'storageBucket' : "test-case-88b02.appspot.com",
+    'messagingSenderId' : "222151653121",
+    'appId': "1:222151653121:web:297b4d553a246ce22dcd00"}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+# db = firebase.database()
+
+auth = firebase.auth()
+
+# storage = firebase.storage()
 
 
-
+email = input("Enter Your Email: ")
+password = input("Enter Your Password: ")
+try:
+    auth.sign_in_with_email_and_password(email,password)
+    print("Logged In Succesfully.")
+except:
+    print("Invalid User or password, Try Again!")
 
 
 
