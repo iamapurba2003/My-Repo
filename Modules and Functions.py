a348ddee9b647bbdb7672bbfb344b1fd9571d244
Himangshu De <dehimangshu2020@gmail.com>
2021-02-22 18:37:35 +0530

    Modified Modules and Functions.py 1837_22022021

---
+++ Modules and Functions.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import ttk
+
+# def greet():
+#     print(f"Hello, {userName.get() or 'World'}!")
+
+root = tk.Tk()
+
+
+
+
+
+
+
+
+
+
+# root.geometry("600x400")
+
+# userName = tk.StringVar()
+
+# rectangle_1 = tk.Label(root, text = "Rectangle 1", bg = "green", fg = "white")
+# rectangle_1.pack(side = "left", ipadx=10, ipady=10,fill = "both", expand=True)
+
+# rectangle_2 = tk.Label(root, text = "Rectangle 2", bg = "red", fg = "white")
+# rectangle_2.pack(side = "top",ipadx=10, ipady=10,fill = "both", expand=True)
+
+# rectangle_3 = tk.Label(root, text = "Rectangle 3", bg = "black", fg = "white")
+# rectangle_3.pack(side = "left", ipadx=10, ipady=10,fill = "both", expand=True)
+
+
+
+# nameLabel = ttk.Label(root, text = "Name: ")
+# nameLabel.pack(side="left", padx=(0,10))
+# nameEntry = ttk.Entry(root, width=15, textvariable=userName)
+# nameEntry.pack(side="left")
+# nameEntry.focus()
+
+# greet_button = ttk.Button(root, text = "Greet", command=greet)
+# greet_button.pack(side = "left")
+
+# quitButton = ttk.Button(root, text = "Quit", command=root.destroy)
+# quitButton.pack(side = "right")
+
+root.mainloop()
+
+
