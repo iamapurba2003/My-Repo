9d347ca58d5e506c2a63b762d1721d893132fb78
Himangshu De <dehimangshu2020@gmail.com>
2021-04-17 12:05:08 +0530

    Modified the NotePad[Beta].py with latest changes and now this project is alpha.

--- Tkinter Projects/NotePad[Beta].py
+++ Tkinter Projects/NotePad[Beta].py
@@ -1,11 +1,10 @@
 from tkinter import *
 from tkinter.ttk import *
 textContent = ""
-fileName = ""
 window = Tk()
-
+window.title("FileName")
 text = StringVar()
-fileName = text.get()
+
 newLabel = Label(window, text="Enter file name(with extension): ")
 newLabel.grid(row=0, column=0)
 
@@ -16,22 +15,26 @@ newLabelText.focus()
 okButton = Button(window, text="Ok", command=window.destroy)
 okButton.grid()
 window.mainloop()
-
 fileName = text.get()
-print(fileName)
 
 window1 = Tk()
-textField = Text(window1, height=44, width=168)
-textField.grid(row=0, column=0)
+window1.title(fileName)
 
+textField = Text(window1, height=44, width=168)
+textField.grid(row=1, column=0)
+
+def save():
+    file = open(fileName, "w")
+    file.write(textField.get("1.0", "end"))
+    file.close()
+
+
+saveButton = Button(window1, text="Save", command=save)
+saveButton.grid(row=0, column=0)
 scrollBar = Scrollbar(window1, orient="vertical", command=textField.yview)
-scrollBar.grid(row=0, column=1,sticky="ns")
+scrollBar.grid(row=1, column=1,sticky="ns")
 textField["yscrollcommand"] = scrollBar.set
-textContent = textField.get("1.0", "end")
 
 window1.mainloop()
 
-file = open(fileName, "w")
-file.write(textContent)
-file.close()
 print(textContent)
