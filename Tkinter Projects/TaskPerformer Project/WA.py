83cfd6790a3508694d9b606e0c4e28c2f0e06049
Apurba Ghosh <71200139+iamapurba2003@users.noreply.github.com>
2021-05-27 18:31:41 +0530

    Added main()
    Problem will be resolved if the linkage of the different functions could be done from the main() function otherwise everything else is fine.

--- Tkinter Projects/TaskPerformer Project/WA.py
+++ Tkinter Projects/TaskPerformer Project/WA.py
@@ -4,15 +4,7 @@ import pywhatkit as kit
 import tkinter.messagebox as mb
 
 # def main():
-new = Tk()
-new.geometry('300x300')
-new.title('Send WA Message')
 
-phoneNumber = StringVar()
-hourTime = IntVar()
-minTime = IntVar()
-delayTime = IntVar()
-message = 0
 def receive(event='<Return>'):
     Label(new, text='Enter message the message for the recipient:').place(x=0,y=70)
     global message
@@ -47,14 +39,6 @@ def senderButton():
     Button(new, text='Send!', command=send).place(x=220, y= 125)
 
 
-Label(new, text="Enter recipient's no.:").place(x=0, y=0)
-Label(new, text="Please enter the phone number \nalong with the Country's ISD Code",foreground='red').place(y=20)
-
-phone = Text(new,  width=25, height=1)
-phone.place(x = 115)
-phone.bind('<Return>',receive)
-
-
 
 def timeSet(event="<Return>"):
     Label(new, text='HH:MM').place(x=0,y=200)
@@ -76,9 +60,39 @@ def timeSet(event="<Return>"):
 
     Button(new, text='Ok',command=senderButton).place(x=60,y=250)
 
+    # Prototye for the method calling from another Script, does not contribute when running this script
+def main():    
+    new = Tk()
+    new.geometry('300x300')
+    new.title('Send WA Message')
+    phoneNumber = StringVar()
+    hourTime = IntVar()
+    minTime = IntVar()
+    delayTime = IntVar()
+    message = 0
+    Label(new, text="Enter recipient's no.:").place(x=0, y=0)
+    Label(new, text="Please enter the phone number \nalong with the Country's ISD Code",foreground='red').place(y=20)
 
-    
+    phone = Text(new,  width=25, height=1)
+    phone.place(x = 115)
+    phone.bind('<Return>',receive)
+    new.mainloop()
 
+    # Solely, contributes while running this Script, does not does anything when a module is called
+if __name__ == "__main__":
+    new = Tk()
+    new.geometry('300x300')
+    new.title('Send WA Message')
+    phoneNumber = StringVar()
+    hourTime = IntVar()
+    minTime = IntVar()
+    delayTime = IntVar()
+    message = 0
+    Label(new, text="Enter recipient's no.:").place(x=0, y=0)
+    Label(new, text="Please enter the phone number \nalong with the Country's ISD Code",foreground='red').place(y=20)
 
-new.mainloop()
+    phone = Text(new,  width=25, height=1)
+    phone.place(x = 115)
+    phone.bind('<Return>',receive)
+    new.mainloop()
 # main()
