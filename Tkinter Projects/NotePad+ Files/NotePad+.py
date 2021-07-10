9eb75cdc5c2f8d8ed1da9df2ab6a86fd5d64560e
Himangshu De <dehimangshu2020@gmail.com>
2021-07-11 01:26:58 +0530

    Modified NotePad+.py file

--- Tkinter Projects/NotePad+ Files/NotePad+.py
+++ Tkinter Projects/NotePad+ Files/NotePad+.py
@@ -2,22 +2,34 @@ from tkinter import *
 import datetime as dt
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
+import tkinter.font 
+from tkinter.scrolledtext import ScrolledText
 import webbrowser
+import Font_NotePad as fnp
 
 # A new window with text field and other attributes
 base = Tk()
-
+Store = "0"
+Size = 0
 # Tkinter window configurations
 base.geometry('500x300')
 base.minsize(200,200)
-base.iconbitmap("D:/Documents/Git Folder/Tkinter Projects/NotePad+.ico")
+# base.iconbitmap("D:/Documents/Git Folder/Tkinter Projects/NotePad+.ico")
 fileName = 'Untitled'
 base.title(f"{fileName} - NotePad+")
 
+Store = StringVar()
+status = 0
 
 # Text Field
-textField = Text(base, height=10, width=13)
-textField.pack(side='left', fill=BOTH, expand=True)
+textField = ScrolledText(base, height=1, width=2,font=('Arial',16))
+textField.pack(anchor=N, fill=BOTH, expand=True)
+textField.config(wrap='none')
+
+#ScrollBars
+scrollBar = Scrollbar(base, orient="horizontal", command=textField.xview)
+scrollBar.pack(side=BOTTOM, anchor=S, fill=X)
+textField["xscrollcommand"] = scrollBar.set
 
 
 def saveasFile(event='<Control-S>'):
@@ -52,14 +64,23 @@ def saveFile(event='<Control-s>'):
         base.title(f"{fileName} -- NotePad+ -- Saved @ {dateandtime.strftime('%H:%M:%S')}")
 
 
+def checker(event="none"):
+    global fileName
+    base.title(f"{fileName[-1]}* -- NotePad+")
+
 def openFile(event='<Control-o>'):
     global fileName
     fileOpen = fd.askopenfile()
     mainFile = open(fileOpen.name,'r')
     fileRead = mainFile.read()
-    textField.insert(INSERT, f'{fileRead}')
+    if textField.get != "":
+        textField.delete(1.0, END)
+        textField.insert(1.0, f'{fileRead}')
+    else:
+        textField.insert(1.0, fileRead)
     fileName = fileOpen.name.split('/')
     base.title(f'Opened: \'{fileName[-1]}\' -- NotePad+')
+    textField.bind("<Key>", checker)
 
 
 def about(event=None):
@@ -71,28 +92,169 @@ def about(event=None):
         callback()
     
 
-def quit(event='<Control-q>'):
-    base.quit()
+def dark():
+    global status
+    _status = status
+    if _status == 1:
+        textField.config(bg='white', fg='black', insertbackground='black')
+        status=0
+
+    if _status == 0:
+        textField.config(bg='black', fg='yellow', insertbackground='yellow')
+        status = 1
+        
+
+def font():
+    app = Toplevel()
+    app.geometry("410x340")
+    app.resizable(False, False)
+    # app.attributes("-toolwindow", True)
+    app.attributes("-toolwindow", True)
+    Value =12
+    Name = "Arial"
+    # _theme = theme
+    # Function Defs
+    def Mode():
+        # nonlocal _theme
+        if status==1:
+            app.config(background='#4a4a4a')
+            FontFamList.config(bg="#828282", fg="yellow")
+            FontFamLabel.config(fg='yellow', bg="#4a4a4a")
+            FontSizeLabel.config(fg="yellow",bg="#4a4a4a")
+            FontSizeList.config(fg='yellow', bg='#828282')
+            okButton.config(bg="#4a4a4a", fg='yellow')
+            SampleFrame.config(bg="#4a4a4a",fg="yellow")
+            LabelText.config(bg="#4a4a4a", fg="yellow")
+            FontFamFrame.config(bg="#4a4a4a")
+            FontSizeFrame.config(bg="#4a4a4a")
+        
+        if status == 0:
+            app.config(background='white')
+            FontFamList.config(bg="white", fg="black")
+            FontFamLabel.config(fg='black', bg="white")
+            FontSizeLabel.config(fg="black",bg="white")
+            FontSizeList.config(fg='black', bg='white')
+            okButton.config(bg="white", fg="black")
+            SampleFrame.config(bg="white",fg="black")
+            LabelText.config(bg="white", fg="black")
+            FontFamFrame.config(bg="white")
+            FontSizeFrame.config(bg="white")
+        
+        
+
+    def updater(name, size):
+        nonlocal Value, Name
+        LabelText.config(font=(f'{name}', size))
+        Value, Name = size, name
+        return Value, Name
+        
+    def save():
+        nonlocal Value, Name
+        textField.config(font=(Name, Value))
+        app.destroy()
+
+    def FontGet(event="None"):
+        # For size & Family
+        nonlocal Value, Name
+        siz = FontSizeList.curselection()
+        fam = FontFamList.curselection()
+        for i in siz:
+            Value = FontSizeList.get(i)
+
+        for i in fam:
+            Name = FontFamList.get(i)
+
+        updater(name=Name, size=int(Value))
+        
+
+    SampleFrame = LabelFrame(app, text="Sample", height=30, width=30)
+    SampleFrame.pack(side=TOP, anchor=NW, fill=X)
+
+
+    names=list(tkinter.font.families(app))
+    FontFamVals = StringVar(value=tuple(names))
+    FontSizeVals= StringVar(value=tuple([8,9,10,11,12,14,16,18,20,22,24,26,28,36,48,72]))
+
+    FontFamFrame = Frame(app)
+    FontFamFrame.pack(side=LEFT, anchor=N)
+
+    FontFamLabel = Label(FontFamFrame, text="Font Family:")
+    FontFamLabel.pack(anchor=NW)
+
+
+
+    # Config/Attrib for Font Family Frame and Widgets
+
+    FontFamScrl = Scrollbar(FontFamFrame, orient=VERTICAL)
+    FontFamScrl.pack(side=RIGHT, fill=Y)
+
+    FontFamList = Listbox(FontFamFrame, listvariable=FontFamVals, height=10, exportselection=False,width=35)
+    FontFamList.pack(anchor=W)
+    FontFamList.bind("<<ListboxSelect>>", FontGet)
+    FontFamList.config(yscrollcommand=FontFamScrl.set)
+    FontFamScrl.config(command=FontFamList.yview)
+
+
+    # Config/Attrib for Font Size Frame and Widgets
+    FontSizeFrame = Frame(app)
+    FontSizeFrame.pack(side=RIGHT, anchor=N)
+    FontSizeLabel = Label(FontSizeFrame, text="Font Size:")
+    FontSizeLabel.pack(side=TOP, anchor=NW)
+
+    FontSizeList =  Listbox(FontSizeFrame, listvariable=FontSizeVals, height=10, exportselection=False)
+    FontSizeList.pack(side=LEFT, anchor=W)
+    FontSizeList.bind("<<ListboxSelect>>", FontGet)
+
+    FontSizeScrl = Scrollbar(FontSizeFrame, orient=VERTICAL)
+    FontSizeScrl.pack(side=RIGHT, fill=Y)
+    FontSizeList.config(yscrollcommand=FontSizeScrl.set)
+    FontSizeScrl.config(command=FontSizeList.yview)
+
+
+
+    LabelText = Label(SampleFrame, text="Sample Text", font=("Arial", 12))
+    LabelText.pack()
+    okButton = Button(app, text="OK", command=save)
+    okButton.pack(anchor=CENTER)
+    Mode()
+    
+    app.mainloop()
+
+
+def onClose(event="none"):
+    message = mb.askyesnocancel("Exit", "Do you want to save this file and QUIT?")
+
+    if message==True:
+        saveasFile()
+        base.destroy()
+    if message==False:
+        base.destroy()
+
+
 
 
 # Menu widget attributes
-menubar = Menu(base)
-fileMenu = Menu(menubar, tearoff=0)
-
+menubar = Menu(base, background='orange', foreground='black')
+fileMenu = Menu(menubar, tearoff=0, background="#b0b0b0", foreground="black")
+themeMenu = Menu(menubar,tearoff=0)
+fontMenu = Menu(menubar, tearoff=0, bg="#b0b0b0", fg="black")
+# Menu Bar File Option Attribs.
+menubar.add_cascade(label='File', menu=fileMenu)
 fileMenu.add_command(label='Save         Ctrl+S', command=saveFile)
 fileMenu.add_command(label='Save As    Ctrl+Shift+S', command=saveasFile)
 fileMenu.add_command(label='Open        Ctrl+O', command=openFile)
 fileMenu.add_separator()
-fileMenu.add_command(label='Exit           Ctrl+Q', command=quit)
+fileMenu.add_command(label='Exit           Ctrl+Q', command=onClose)
 
+# Menu Bar Theme Option Attribs.
+menubar.add_command(label="Theme", command=dark)
 
-menubar.add_cascade(label='File', menu=fileMenu)
+fontMenu.add_command(label="Font", command=font)
+menubar.add_cascade(label='Format', menu=fontMenu)
+
 menubar.add_cascade(label='About', command=about)
 
 
-scrollBar = Scrollbar(base, orient="vertical", command=textField.yview)
-scrollBar.pack(side='right', fill=Y)
-textField["yscrollcommand"] = scrollBar.set
 
 base.config(menu=menubar)
 
@@ -100,7 +262,10 @@ base.config(menu=menubar)
 base.bind('<Control-s>',saveFile)
 base.bind('<Control-S>',saveasFile)
 base.bind('<Control-o>',openFile)
-base.bind('<Control-q>',quit)
+base.bind('<Control-q>',onClose)
+base.bind('<Control-T>', dark)
+# base.bind('<Key>', auto)
 
+base.protocol("WM_DELETE_WINDOW", onClose)
 # Running the main tkinter window
 base.mainloop()
